66330139a9f0f4c11cfac135858d192cb4399089
Shravan Asati <delunator.one@yahoo.com>
2021-01-13 19:23:41 +0530

    trying to fix binod.meme

--- discord_bot.py
+++ discord_bot.py
@@ -9,21 +9,42 @@
 # binod.meme done
 # binod.suggest done
 
-import discord, requests, json, wikipedia, random, smtplib, datetime, subprocess
+import datetime
+import json
+import random
+import smtplib
+import subprocess
+
+import discord
+import requests
+import wikipedia
 from bs4 import BeautifulSoup
 
+
+def send_mail(subject, body):
+    server = smtplib.SMTP('smtp.mail.yahoo.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('delunator.one@yahoo.com', "ytxyqkendtnngqib")
+    server.sendmail('delunator.one@yahoo.com',
+                    'delunator.one@yahoo.com', f"Subject: {subject}\n\n{body}")
+    server.quit()
+
+
 client = discord.Client()
+
 
 @client.event
 async def on_ready():
     print('Bot has logged in as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
     command = message.content.lower()
 
     if message.author == client.user:
-        return 
+        return
 
     help = "Hello I am a bot made by Shravan! You can do some really amazing things by following commands.\n\n binod.help = Bring this help message\n\n binod.binod = BINOD\n\n binod.news = Top news headlines of current time\n\n binod.weather <place> = Real time weather of <place> (Replace <place> with the place whose weather you want to know)\n\n binod.wikipedia <search> = Searches the wikipedia for <search> (Again replace the <search> with your query)\n\n binod.joke = Call a random joke \n\n binod.corona = Get live status of confirmed corona cases of India \n\n binod.meme = Get a random meme \n\n binod.suggest <suggestion> = Suggest a new feature to be added to the bot \n\n"
 
@@ -45,13 +66,13 @@ async def on_message(message):
         for article in arts:
             string += f"{article['title']}\n"
 
-            if a==2:
+            if a == 2:
                 break
             else:
                 a += 1
                 continue
 
-        await message.channel.send(string) 
+        await message.channel.send(string)
 
     elif command.startswith("binod.weather"):
         query = command.replace("binod.weather", "")
@@ -73,13 +94,12 @@ async def on_message(message):
             print(e)
             await message.channel.send("Couldn't search that!")
 
-
     elif command.startswith("binod.wikipedia"):
         try:
             await message.channel.send("Searching through the wikipedia for your query... Hang on...")
             query = command.replace('binod.wikipedia', "")
             query = query.lstrip()
-            result = wikipedia.summary(query, sentences = 2)
+            result = wikipedia.summary(query, sentences=2)
             material = f"According to wikipedia...\n{result}"
             await message.channel.send(material)
 
@@ -87,64 +107,61 @@ async def on_message(message):
             print(e)
             await message.channel.send("Some error occurred. Sorry for your inconvenience!")
 
-
     elif command.startswith("binod.corona"):
-        r = requests.get(f'https://api.covid19api.com/country/india/status/confirmed/live')
-
+        r = requests.get(
+            f'https://api.covid19api.com/country/india/status/confirmed/live')
 
         try:
             parser = json.loads(r.text)
             l = parser[::-1]
             today = (l[0])
-            material = (f"There are {today['Cases']} confirmed corona cases in India.")
+            material = (
+                f"There are {today['Cases']} confirmed corona cases in India.")
             await message.channel.send(material)
         except Exception as e:
             print(e)
             await message.channel.send("Couldn't do that at the moment!")
 
-        
     elif command.startswith('binod.joke'):
-        types = ["knock-knock", 'jod', 'blonde','animal']
+        types = ["knock-knock", 'jod', 'blonde', 'animal']
         url = f"https://api.jokes.one/jod?category={random.choice(types)}"
         r = requests.get(url).text
         parser = json.loads(r)
         joke = parser['contents']['jokes'][0]['joke']['text']
         await message.channel.send(f"Alright, here's a one:\n{joke}")
-        
 
     elif command.startswith('binod.meme'):
-        r = requests.get("https://imgflip.com/")
-        soup = BeautifulSoup(r.content, "html5lib")
+        try:
+            r = requests.get("https://imgflip.com/")
+            soup = BeautifulSoup(r.content, "html5lib")
 
-        images = soup.find_all("img")
-        r = random.choice(images)
-        link = (r["src"])
-        ext = link.split(".")[-1]
+            images = soup.find_all("img")
+            r = random.choice(images)
+            link = (r["src"])
+            ext = link.split(".")[-1]
 
-        location = f"meme.{ext}"
-        data = requests.get("http:"+link).content
-        with open(location, "wb") as f:
-            f.write(data)
+            location = f"meme.{ext}"
+            data = requests.get("http:"+link).content
+            with open(location, "wb") as f:
+                f.write(data)
 
-        await message.channel.send(file=discord.File(location))
+            await message.channel.send(file=discord.File(location))
+
+        except Exception as e:
+            send_mail("Binod bot notification", e)
 
     elif command.startswith("binod.suggest"):
         suggestion = command.replace("binod.suggest", "")
         author = message.author.mention
         author_name = message.author
         text = f"{author_name}: {suggestion}\n"
-        
+
         subject = f"{author_name}'s suggestion for binod-bot"
         time = datetime.datetime.now().strftime("%D %H:%m")
         body = f"Sir, \nOn {time}, {author_name} suggested following feature for the binod bot on discord. \n\n {author_name}: {suggestion}"
 
         try:
-            server = smtplib.SMTP('smtp.mail.yahoo.com', 587)
-            server.ehlo()
-            server.starttls()
-            server.login('delunator.one@yahoo.com', "ytxyqkendtnngqib")
-            server.sendmail('delunator.one@yahoo.com', 'delunator.one@yahoo.com', f"Subject: {subject}\n\n{body}")
-            server.quit()
+            send_mail(subject, body)
             await message.channel.send(f"{author} Your suggestion has been recorded!")
 
         except Exception as e:
