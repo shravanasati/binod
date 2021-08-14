049333df85c9d9b27dce97953dff34d30ac4d333
Shravan Asati <dev.shravan@protonmail.com>
2021-08-14 19:28:40 +0530

    make the bot public

--- discord_bot.py
+++ discord_bot.py
@@ -1,34 +1,11 @@
-# TODO:
-# binod.commands done
-# binod.news done
-# binod.weather done
-# binod.wikipedia done
-# binod.corona done
-# binod.ipl
-# binod.joke done
-# binod.meme done
-# binod.suggest done
-
-import datetime
 import json
+import os
 import random
-import smtplib
 import subprocess
-
 import discord
 import requests
 import wikipedia
 from bs4 import BeautifulSoup
-
-
-def send_mail(subject, body):
-    server = smtplib.SMTP('smtp.mail.yahoo.com', 587)
-    server.ehlo()
-    server.starttls()
-    server.login('delunator.one@yahoo.com', "ytxyqkendtnngqib")
-    server.sendmail('delunator.one@yahoo.com',
-                    'delunator.one@yahoo.com', f"Subject: {subject}\n\n{body}")
-    server.quit()
 
 
 client = discord.Client()
@@ -55,7 +32,7 @@ async def on_message(message):
         await message.channel.send("BINOD")
 
     elif command.startswith("binod.news"):
-        url = "https://newsapi.org/v2/top-headlines?sources=the-times-of-india&apiKey=fbddfcc479954110ba532f7b45dc41d2"
+        url = "https://newsapi.org/v2/top-headlines?sources=the-times-of-india&apiKey={}".format(os.environ['NEWS_API_KEY'])
         news = requests.get(url).text
         news_dict = json.loads(news)
         arts = news_dict['articles']
@@ -80,7 +57,7 @@ async def on_message(message):
         query = query.capitalize()
 
         try:
-            url = f"http://api.weatherapi.com/v1/current.json?key=2f081a6878a747a5be135553200709&q={query}"
+            url = "http://api.weatherapi.com/v1/current.json?key={}&q={}".format(os.environ['WEATHER_API_KEY'], query)
 
             r = requests.get(url).text
             a = json.loads(r)
@@ -88,7 +65,7 @@ async def on_message(message):
             condition = current_weather['condition']
             text = condition['text']
 
-            await message.channel.send(f"The current tempearture in {query} is {current_weather['temp_c']}. The weather condition in {query} is {text.lower()}.")
+            await message.channel.send(f"The current temperature in {query} is {current_weather['temp_c']}. The weather condition in {query} is {text.lower()}.")
 
         except Exception as e:
             print(e)
@@ -149,35 +126,21 @@ async def on_message(message):
             await message.channel.send(file=discord.File(location))
 
         except Exception as e:
-            await message.channel.send("Some error occured!")
-            send_mail("Binod bot notification", e)
-
-    elif command.startswith("binod.suggest"):
-        suggestion = command.replace("binod.suggest", "")
-        author = message.author.mention
-        author_name = message.author
-        text = f"{author_name}: {suggestion}\n"
-
-        subject = f"{author_name}'s suggestion for binod-bot"
-        time = datetime.datetime.now().strftime("%D %H:%m")
-        body = f"Sir, \nOn {time}, {author_name} suggested following feature for the binod bot on discord. \n\n {author_name}: {suggestion}"
-
-        try:
-            send_mail(subject, body)
-            await message.channel.send(f"{author} Your suggestion has been recorded!")
-
-        except Exception as e:
+            await message.channel.send("Some error occurred!")
             print(e)
-            await message.channel.send("Couldn't connect at the moment! Try again later.")
 
     elif command.startswith("/exec"):
         try:
-            query = command.replace("/exec", "").lstrip()
-            output = subprocess.check_output(query, shell=True).decode("utf-8")
-            await message.channel.send(output)
+            if message.author.name == "Shravan" and message.author.discriminator == "6942":
+                query = command.replace("/exec", "").lstrip()
+                output = subprocess.check_output(
+                    query, shell=True).decode("utf-8")
+                await message.channel.send(output)
+            else:
+                await message.channel.send("You're not allowed to do that!")
 
         except subprocess.CalledProcessError as e:
             await message.channel.send(e)
 
 
-client.run("NzYwNTE2MTc5MDQ2ODI1OTk0.X3NL4g.zRL8-FwDNSzL6YgXB2mbzHeWx6Y")
+client.run(os.getenv("DISCORD_BOT_TOKEN"))
