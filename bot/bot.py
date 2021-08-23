57f0a4774e71eaf37188300d4c40787647b55816
Shravan Asati <dev.shravan@protonmail.com>
2021-08-24 00:01:13 +0530

    ✨ remote code execution

--- bot/bot.py
+++ bot/bot.py
@@ -9,31 +9,34 @@ from src.insult import insult
 from src.github import github_user, github_repo
 from src.memes import get_meme
 from src.shorten import shorten
+from src.compile import CodeExecutor
 from enchant.utils import levenshtein
 
 
 client = discord.Client()
-
+rce = CodeExecutor()
 
 @client.event
 async def on_ready():
     print('Bot has logged in as {0.user}'.format(client))
 
-def cembed(title="", description="", thumbnail=client.user.avatar_url_as(format="png"), picture="", color=discord.Color.dark_theme()):
-    embed = discord.Embed()
-    if color != discord.Color.dark_theme():
-        embed = discord.Embed(color=discord.Color(value=color))
-    if title != "":
-        embed.title = title
-    if description != "":
-        embed.description = description
-    if thumbnail != "":
-        embed.set_thumbnail(url=thumbnail)
-    if picture != "":
-        embed.set_image(url=picture)
-    return embed
 
-color=discord.Color.from_rgb(100,230,160)
+# def cembed(title="", description="", thumbnail=client.user.avatar_url_as(format="png"), picture="", color=discord.Color.dark_theme()):
+#     embed = discord.Embed()
+#     if color != discord.Color.dark_theme():
+#         embed = discord.Embed(color=discord.Color(value=color))
+#     if title != "":
+#         embed.title = title
+#     if description != "":
+#         embed.description = description
+#     if thumbnail != "":
+#         embed.set_thumbnail(url=thumbnail)
+#     if picture != "":
+#         embed.set_image(url=picture)
+#     return embed
+
+
+color = discord.Color.from_rgb(100, 230, 160)
 
 
 @client.event
@@ -46,13 +49,14 @@ async def on_message(message):
     help_message = "Hello, I am Binod, a bot made by Shravan. The following are the commands:\n\n binod.help = Show this help message\n\n binod.binod = BINOD\n\n binod.news = Top news headlines of the moment\n\n binod.weather <place> = Real time weather of <place> \n\n binod.wiki <search> = Searches the wikipedia for <search> \n\n binod.joke = Get a random joke \n\n binod.corona = Get live status of confirmed corona cases in India \n\n binod.meme = Get a random meme \n\n binod.suggest <suggestion> = Suggest a new feature to be added to the bot \n\n"
 
     if command.startswith('binod.help'):
-        await message.channel.send(embed=cembed(title="Help",description=help_message,color=color))
+        await message.channel.send(embed=cembed(title="Help", description=help_message, color=color))
 
     elif command.startswith('binod.binod'):
         await message.channel.send("BINOD")
 
     elif command.startswith("binod.news"):
-        url = "https://newsapi.org/v2/top-headlines?sources=the-times-of-india&apiKey={}".format(os.environ['NEWS_API_KEY'])
+        url = "https://newsapi.org/v2/top-headlines?sources=the-times-of-india&apiKey={}".format(
+            os.environ['NEWS_API_KEY'])
         news = requests.get(url).text
         news_dict = json.loads(news)
         arts = news_dict['articles']
@@ -69,7 +73,7 @@ async def on_message(message):
                 a += 1
                 continue
 
-        await message.channel.send(embed=cembed(title="News",description=string,color=color))
+        await message.channel.send(embed=cembed(title="News", description=string, color=color))
 
     elif command.startswith("binod.weather"):
         query = command.replace("binod.weather", "")
@@ -77,7 +81,8 @@ async def on_message(message):
         query = query.capitalize()
 
         try:
-            url = "http://api.weatherapi.com/v1/current.json?key={}&q={}".format(os.environ['WEATHER_API_KEY'], query)
+            url = "http://api.weatherapi.com/v1/current.json?key={}&q={}".format(
+                os.environ['WEATHER_API_KEY'], query)
 
             r = requests.get(url).text
             a = json.loads(r)
@@ -89,20 +94,20 @@ async def on_message(message):
 
         except Exception as e:
             print(e)
-            await message.channel.send(embed=cembed(title="Weather",description=f"Cannot search for a place named {query}!",color=color))
+            await message.channel.send(embed=cembed(title="Weather", description=f"Cannot search for a place named {query}!", color=color))
 
     elif command.startswith("binod.wiki"):
         try:
-            await message.channel.send(embed=cembed(description="Searching through the wikipedia for your query... Hang on...",color=color))
+            await message.channel.send(embed=cembed(description="Searching through the wikipedia for your query... Hang on...", color=color))
             query = command.replace('binod.wiki', "")
             query = query.lstrip()
             result = wikipedia.summary(query, sentences=2)
             material = f"According to wikipedia:\n{result}"
-            await message.channel.send(embed=cembed(title="Wiki",description=material,color=color))
+            await message.channel.send(embed=cembed(title="Wiki", description=material, color=color))
 
         except Exception as e:
             print(e)
-            await message.channel.send(embed=cembed(title="Wiki",description="Some error occurred. Sorry for the inconvenience!",color=color))
+            await message.channel.send(embed=cembed(title="Wiki", description="Some error occurred. Sorry for the inconvenience!", color=color))
 
     elif command.startswith("binod.corona"):
         country = command.replace('binod.corona', '').lstrip().rstrip()
@@ -115,7 +120,7 @@ async def on_message(message):
             today = (l[0])
             material = (
                 f"There are {today['Cases']} confirmed corona cases in {country.capitalize()}.")
-            await message.channel.send(embed=cembed(title="Corona",description=material,color=color))
+            await message.channel.send(embed=cembed(title="Corona", description=material, color=color))
         except Exception as e:
             print(e)
             await message.channel.send("Couldn't do that at the moment!")
@@ -126,7 +131,7 @@ async def on_message(message):
         r = requests.get(url).text
         parser = json.loads(r)
         joke = parser['contents']['jokes'][0]['joke']['text']
-        await message.channel.send(embed=cembed(title="Joke",description=f"Alright, here's a one:\n{joke}",color=color))
+        await message.channel.send(embed=cembed(title="Joke", description=f"Alright, here's a one:\n{joke}", color=color))
 
     elif command.startswith('binod.meme'):
         try:
@@ -154,7 +159,8 @@ async def on_message(message):
             await message.channel.send("Invalid query!")
 
     elif command.startswith("binod.shorten"):
-        queries = command.replace("binod.shorten", "").lstrip().rstrip().split(" ")
+        queries = command.replace(
+            "binod.shorten", "").lstrip().rstrip().split(" ")
         if len(queries) == 1:
             await message.channel.send(shorten(queries[0]))
         elif len(queries) == 2:
@@ -176,6 +182,33 @@ async def on_message(message):
 
         except subprocess.CalledProcessError as e:
             await message.channel.send(e)
+
+    elif command.startswith("binod.run"):
+        split = command.replace("binod.run", "").lstrip().rstrip().split("\n")
+        language = split[0].strip()
+
+        actual_code = ""
+        for i in split[1:]:
+            if not i.startswith("```"):
+                actual_code += i+"\n"
+
+        resp = rce.execute_code(language, actual_code)
+        result = f"{message.author.mention}, here's your code execution result: \n"
+        result += f"Language: {resp.language} \n"
+        result += f"Exit Code: {resp.exit_code} \n"
+        result += f"Output:\n```{resp.output}```"
+        await message.channel.send(result)
+
+    elif command.startswith("binod.langs"):
+        results = rce.runtimes
+        resp_text = f"{message.author.mention}, here's the list of supported languages: \n"
+        for i in results:
+            lang = i["language"]
+            version = i["version"]
+            alias = ",".join(i["aliases"])
+            resp_text += f"{lang},{alias} - {version}\n"
+
+        await message.channel.send(resp_text)
 
 
 client.run(os.getenv("DISCORD_BOT_TOKEN"))
---
+++ bot/src/compile.py
@@ -0,0 +1,102 @@
+import requests
+import traceback
+import json
+from typing import List, Dict, Union
+from functools import lru_cache
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class PistonResponse:
+    """
+    Piston API response class.
+    """
+    language: str
+    exit_code: int
+    output: str
+
+
+class CodeExecutor:
+    """
+    Base class for the remote code execution.
+    """
+
+    def __init__(self) -> None:
+        self.runtimes = self.__get_runtimes()
+
+    @staticmethod
+    def __get_runtimes() -> List[Dict[str, Union[str, List[str]]]]:
+        """
+        Returns a list of all available runtimes offered by the piston api.
+        """
+        try:
+            runtime_url = "https://emkc.org/api/v2/piston/runtimes"
+            r = requests.get(runtime_url)
+            data = json.loads(r.text)
+            runtimes: List[Dict[str, Union[str, List[str]]]] = []
+            for langs in data:
+                runtimes.append({
+                    "language": langs["language"],
+                    "version": langs["version"],
+                    "aliases": [i for i in langs["aliases"]]
+                })
+
+            return runtimes
+
+        except Exception as e:
+            traceback.print_exception(e)
+            return []
+
+    @lru_cache(maxsize=50)
+    def execute_code(self, language: str, code: str) -> PistonResponse:
+        """
+        Executes the given code in the given language and version.
+        """
+        try:
+            execute_url = "https://emkc.org/api/v2/piston/execute"
+            all_langs = [runtime["language"] for runtime in self.runtimes]
+            _aliases = [runtime["aliases"] for runtime in self.runtimes]
+            aliases = [i for sublist in _aliases for i in sublist]
+
+            if language not in all_langs and language not in aliases:
+                return PistonResponse(language, -1, f"Language {language} is not supported.")
+
+            for runtime in self.runtimes:
+                if runtime["language"] == language or language in runtime["aliases"]:
+                    version = runtime["version"]
+                    break
+
+            else:
+                return PistonResponse(language, -1, f"Language {language} is not supported.")
+
+            payload = {
+                "language": language,
+                "version": version,
+                "files": [{
+                    "name": "prog",
+                    "content": code,
+                }]
+            }
+
+            resp = requests.post(execute_url, json=payload)
+
+            if resp.status_code == 200:
+                data = json.loads(resp.text)
+
+                run_data = data["run"]
+
+                return PistonResponse(language, run_data["code"], run_data["output"])
+
+            else:
+                return PistonResponse(language, -1, f"Internal error: {resp.status_code}")
+
+        except Exception as e:
+            traceback.print_exception(e)
+            return PistonResponse(language, -1, "Couldn't connect at the moment.")
+
+
+if __name__ == "__main__":
+    rce = CodeExecutor()
+    # print(rce.runtimes)
+    print(rce.execute_code(language="vlang", code="print('hello')"))
+    print(rce.execute_code(language="vlang", code="print('hello')"))
